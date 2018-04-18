6f4b51633d49487ddee0e27dafd0ccb8d75d462a
Jonathan Persaud <JonathanPersaud@jonathansmbp39.home>
2018-04-18 17:58:57 -0400

    create app

--- TakeOff/ViewController.swift
+++ TakeOff/ViewController.swift
@@ -7,12 +7,27 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var darkBlueBG: UIImageView!
+    @IBOutlet weak var powerButton: UIButton!
+    @IBOutlet weak var cloudHolder: UIView!
+    @IBOutlet weak var rocket: UIImageView!
+    @IBOutlet weak var takeOffLbl: UILabel!
+    @IBOutlet weak var onLbl: UILabel!
+    var player : AVAudioPlayer!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let path = Bundle.main.path(forResource: "hustle-on", ofType: "wav")
+        let url = URL(fileURLWithPath: path!, isDirectory: true)
+        do{
+            player = try AVAudioPlayer(contentsOf: url)
+            player.prepareToPlay()
+        }catch{
+            print("Error playing sound!")
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +35,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func powerButtonPressed(_ sender: Any) {
+        cloudHolder.isHidden = false
+        darkBlueBG.isHidden = true
+        powerButton.isHidden = true
+        player.play()
+        UIView.animate(withDuration: 2.3, animations: {
+            self.rocket.frame = CGRect(x: 0, y: 20, width: 414, height: 685)
+        }) { (finished) in
+            self.takeOffLbl.isHidden = false
+            self.onLbl.isHidden = false
+        }
+    }
+    
 
 }
 
